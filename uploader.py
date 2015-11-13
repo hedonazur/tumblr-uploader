ca08fbe1232b7c9a8fc118dc09a42d72628dfb01
hedonazur <hedonazur@hotmail.com>
2015-11-13 23:40:10 +0200

    pep8 checked

--- uploader.py
+++ uploader.py
@@ -1,15 +1,14 @@
 import pytumblr
 import pafy
 import sys
+import json
 from tumblr_keys import consumer_key
 from tumblr_keys import consumer_secret
 from tumblr_keys import token_key
-from tumblr_keys import token_secret 
+from tumblr_keys import token_secret
 from tumblr_keys import youtube_key
-import json
 
-
-#Authenticate via OAuth
+# Authenticate via OAuth
 client = pytumblr.TumblrRestClient(
     consumer_key,
     consumer_secret,
@@ -18,24 +17,24 @@ client = pytumblr.TumblrRestClient(
 )
 
 print (sys.argv)
-titulo= sys.argv[1]
-cuerpofoto=sys.argv[2]
+titulo = sys.argv[1]
+cuerpofoto = sys.argv[2]
 
 if titulo == "sync" and cuerpofoto == "videos":
-    ###videos
+    # videos
 
-    videos = client.posts("perunalainen", type="video", limit=10)
+    videos = client.posts("perunalainen", type="video", limit=20)
     videos_list = videos['posts']
-    last_10=[]
+    last_10 = []
     for video in videos_list:
         last_10.append(video["permalink_url"][-11:])
 
     pafy.set_api_key(youtube_key)
     v = "https://www.youtube.com/playlist?list=PL7rxKO9f1bIguuo1dPQRFu7bIr3eqFcij"
     playlist = pafy.get_playlist(v)
-    tot= len(playlist['items'])
+    tot = len(playlist['items'])
     count = 0
-    ids =[]
+    ids = []
     while count < tot:
         objeto = playlist['items'][count]['playlist_meta']['encrypted_id']
         ids.append(objeto)
@@ -46,22 +45,11 @@ if titulo == "sync" and cuerpofoto == "videos":
             print "all sync"
         else:
             client.create_video('perunalainen', embed="https://www.youtube.com/watch?v="+v)
-            
+
 elif cuerpofoto.endswith(".jpg"):
-	resultado= client.create_photo('perunalainen', state="published", data="/home/carol/Dropbox/tumblr/"+ cuerpofoto, caption=titulo)
-	print (resultado)
-            
+    resultado = client.create_photo('perunalainen', state="published", data="/home/carol/Dropbox/tumblr/" + cuerpofoto, caption=titulo)
+    print (resultado)
+
 else:
-	resultado= client.create_text("perunalainen", state="published", slug="testing-text-posts", title=titulo, body=cuerpofoto)
-	print (resultado)
-            
-     
-
-
-
-
-
-
-
-
-
+    resultado = client.create_text("perunalainen", state="published", slug="testing-text-posts", title=titulo, body=cuerpofoto)
+    print (resultado)
